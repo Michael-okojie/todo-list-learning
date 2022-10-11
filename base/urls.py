b71386551711792bddfcb32e26e272cd0d52df7b
Michael-okojie <michaelokojie1@gmail.com>
2022-10-11 11:15:06 +0100

    added update, create and detail functionalities

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TaskList, TaskDetail
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='task-list'),
-    path('<int:pk>', TaskDetail.as_view(), name='task-detail'),
+    path('<int:pk>/', TaskDetail.as_view(), name='task-detail'),
+    path('create/', TaskCreate.as_view(), name='task-create'),
+    path('<int:pk>/update', TaskUpdate.as_view(), name='task-update'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Task
+from django.urls import reverse_lazy
 
 
 class TaskList(ListView):
@@ -12,4 +13,15 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
-    
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('task-list')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('task-list')
