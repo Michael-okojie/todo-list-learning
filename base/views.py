13e5c5cdc75b751f4f6e125f033d1724cd63975d
Michael-okojie <michaelokojie1@gmail.com>
2022-10-12 04:53:20 +0100

    edited my views to make sure only specific user tasks appers on provision of user login

--- base/views.py
+++ base/views.py
@@ -19,6 +19,12 @@ class TaskListView(LoginRequiredMixin, ListView):
     context_object_name = 'tasks'
     queryset = Task.objects.all()
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+        return context
+
 
 class TaskDetailView(LoginRequiredMixin, DetailView):
     model = Task
@@ -28,13 +34,17 @@ class TaskDetailView(LoginRequiredMixin, DetailView):
 
 class TaskCreateView(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('task-list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreateView, self).form_valid(form)
 
 
 class TaskUpdateView(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('task-list')
 
 
